d0d7ffbc9c13166d869518899590740ae18fdc3b
Oleksandr Shcherbakov <shcherbakov@hlrs.de>
2017-07-24 15:26:29 +0200

    Revert "CATs. Full interfaces file to be overwritten during provisioning"
    These changes are not needed any more.

--- ubuntu/scripts/interfaces.sh
+++ ubuntu/scripts/interfaces.sh
@@ -1,18 +1,4 @@
-cat <<EOF > /etc/network/interfaces
-# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
-
-source /etc/network/interfaces.d/*
-
-# The loopback network interface
-auto lo
-iface lo inet loopback
-
-# The primary network interface
-auto ens3
-iface ens3 inet dhcp
-  up route add gw 192.168.122.1 ens3
-
+cat <<EOF >> /etc/network/interfaces
 # One more network interface
 auto ens1
 iface ens1 inet dhcp
