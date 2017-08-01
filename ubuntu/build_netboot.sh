61ca4df0ac8463136832c2d172b441178718f49f
Oleksandr Shcherbakov <shcherbakov@hlrs.de>
2017-08-01 19:11:15 +0200

    Ubuntu 14.04 minimal (netboot)

---
+++ ubuntu/build_netboot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# get infromations
+read -p 'Enter Username:' userName
+read -p 'Enter password: ' -s password
+
+#imageDir=/images
+indisk="output-ubuntu-14.04-netboot-amd64/packer-ubuntu-14.04-netboot-amd64"
+outdisk="output-ubuntu-14.04-netboot-amd64/packer-ubuntu-14.04-netboot-amd64.img"
+
+# export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
+
+# build the image
+packer build -var "user=${userName}" -var "password=${password}" ubuntu-14.04-netboot.json
+
+
+# cleaning the img
+virt-sysprep -a ${indisk} --firstboot-command 'dpkg-reconfigure openssh-server'
+
+# move and compress the image
+virt-sparsify ${indisk} ${outdisk}  --compress --check-tmpdir ignore
+
+# removing old img
+rm ${indisk}
+
