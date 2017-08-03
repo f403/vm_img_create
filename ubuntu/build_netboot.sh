651956c4185677d5e215ad461d57f0553752929d
Oleksandr Shcherbakov <shcherbakov@hlrs.de>
2017-08-03 13:28:26 +0200

    ubuntu14-min hostname will be requested in the build script

--- ubuntu/build_netboot.sh
+++ ubuntu/build_netboot.sh
@@ -3,6 +3,7 @@
 # get infromations
 read -p 'Enter Username:' userName
 read -p 'Enter password: ' -s password
+read -p 'Enter VM hostname: ' -s vmhostname
 
 #imageDir=/images
 indisk="output-ubuntu-14.04-netboot-amd64/packer-ubuntu-14.04-netboot-amd64"
@@ -11,7 +12,7 @@ outdisk="output-ubuntu-14.04-netboot-amd64/packer-ubuntu-14.04-netboot-amd64.img
 # export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 
 # build the image
-packer build -var "user=${userName}" -var "password=${password}" ubuntu-14.04-netboot.json
+packer build -var "user=${userName}" -var "password=${password}" -var "hostname=${vmhostname}" ubuntu-14.04-netboot.json
 
 
 # cleaning the img
