27cd9aa5c74f1fc251c358e778b4e24c23a25d59
Oleksandr Shcherbakov <shcherbakov@hlrs.de>
2017-08-03 10:38:17 +0200

    Ubuntu sudoers fix

--- ubuntu/scripts/sudoers.sh
+++ ubuntu/scripts/sudoers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$USERNAME ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USERNAME
-chmod 440 /etc/sudoers.d/$USERNAME
-chown root:root /etc/sudoers.d/$USERNAME
+echo "$SUDO_USER ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$SUDO_USER
+chmod 440 /etc/sudoers.d/$SUDO_USER
+chown root:root /etc/sudoers.d/$SUDO_USER
