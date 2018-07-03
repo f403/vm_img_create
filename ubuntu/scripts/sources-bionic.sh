23c333c620f7c539e82686cef8330826c17ed407
Oleksandr Shcherbakov <shcherbakov@hlrs.de>
2018-07-03 21:17:52 +0200

    Using cdimage for ubuntu 18.04

---
+++ ubuntu/scripts/sources-bionic.sh
@@ -0,0 +1,7 @@
+cat <<EOF >> /etc/apt/sources.list
+# Uni Stuttgart Mirror
+deb http://ftp.uni-stuttgart.de/ubuntu/ bionic main restricted universe multiverse
+deb http://ftp.uni-stuttgart.de/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://ftp.uni-stuttgart.de/ubuntu/ bionic-security main restricted universe multiverse
+deb http://ftp.uni-stuttgart.de/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
