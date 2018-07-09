f751a2b1f93abbbafa46fe84a54b6f532c78b616
Oleksandr Shcherbakov <shcherbakov@hlrs.de>
2018-07-09 20:35:10 +0200

    fix. xrdp log permissions

--- ubuntu/scripts/packages-server-desktop.sh
+++ ubuntu/scripts/packages-server-desktop.sh
@@ -15,3 +15,6 @@ crudini
 apt-get -y install $PACKAGES
 echo mate-session> /etc/skel/.xsession
 crudini --set /etc/xrdp/xrdp.ini Globals channel_code false
+touch /var/log/xrdp.log
+chown xrdp:root /var/log/xrdp.log
+chmod 600 /var/log/xrdp.log
